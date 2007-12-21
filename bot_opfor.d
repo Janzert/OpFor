09549c90c30441fe324fb3397d942af26bb4653a
janzert@janzert.com <>
2007-12-21 14:08:31 -0500

    bot_opfor: Switch dog and horse placement in opening setup

--- bot_opfor.d
+++ bot_opfor.d
@@ -481,15 +481,15 @@ class Engine : AEIEngine
     {
         if (ply == 1) // white setup move
         {
-            bestmove = "Ra1 Rb1 Rc1 Rd1 Re1 Rf1 Rg1 Rh1 Ha2 Db2 Cc2 Md2 Ee2 Cf2 Dg2 Hh2";
+            bestmove = "Ra1 Rb1 Rc1 Rd1 Re1 Rf1 Rg1 Rh1 Da2 Hb2 Cc2 Md2 Ee2 Cf2 Hg2 Dh2";
             state = EngineState.MOVESET;
         } else if (ply == 2)
         {
             if (position.pieces[11] == Piece.WELEPHANT)
             {
-                bestmove = "ra8 rb8 rc8 rd8 re8 rf8 rg8 rh8 ha7 db7 cc7 ed7 me7 cf7 dg7 hh7";
+                bestmove = "ra8 rb8 rc8 rd8 re8 rf8 rg8 rh8 da7 hb7 cc7 ed7 me7 cf7 hg7 dh7";
             } else {
-                bestmove = "ra8 rb8 rc8 rd8 re8 rf8 rg8 rh8 ha7 db7 cc7 md7 ee7 cf7 dg7 hh7";
+                bestmove = "ra8 rb8 rc8 rd8 re8 rf8 rg8 rh8 da7 hb7 cc7 md7 ee7 cf7 hg7 dh7";
             }
             state = EngineState.MOVESET;
         } else {
