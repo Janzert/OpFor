2e9110b0d3d14a4d743c35d2f2e2fbb9f3f465cd
janzert@janzert.com <>
2008-02-04 23:39:43 -0500

    Lessen the effect of goal threat on strong rabbits

--- bot_opfor.d
+++ bot_opfor.d
@@ -382,8 +382,8 @@ real rabbit_strength(Position pos, GoalSearch goals, real weak_w, real strong_w)
     const static real[] goalval = [1.0,
           1.0, 1.0, 1.0, 1.0,
           1.0, 1.0, 0.9, 0.9,
-          0.8, 0.8, 0.7, 0.7,
-          0.6, 0.6, 0.5, 0.5];
+          0.8, 0.8, 0.8, 0.8,
+          0.7, 0.7, 0.7, 0.7];
     const static real[] weakgoal = [0,
           0, 0, 0, 0,
           0.1, 0.1, 0.2, 0.2,
