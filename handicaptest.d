05772ef4a60c649fc29de4c841f11a8e65763866
janzert@janzert.com <>
2008-07-25 15:30:12 -0400

    Clean up interface (command line and output) for handicaptest

--- handicaptest.d
+++ handicaptest.d
@@ -14,47 +14,70 @@ int main(char[][] args)
     int wins = 0;
     if (args.length < 2)
     {
-        writefln("usage: handicaptest <side> <handicap pieces>");
+        writefln("usage: handicaptest <handicap pieces>");
         return 1;
     }
-    int side = ifind("wb", args[1][0]);
-    if (side == -1)
+
+    int[13] handicap = [0, 8,2,2,2,1,1, 8,2,2,2,1,1];
+    for (int i = 0; i < args[1].length; i++)
     {
-        writefln("Illegal side given, must be w or b");
-        return 1;
-    }
-    int offset = (side == Side.WHITE) ? 0 : 6;
-    Piece[] handicap;
-    for (int i = 0; i < args[2].length; i++)
-    {
-        int p = ifind(".rcdhme", args[2][i]);
+        int p = find(".RCDHMErcdhme", args[1][i]);
         if (p < 1)
         {
             writefln("Illegal piece given, %s", args[1][i]);
             return 1;
         }
-        handicap.length = handicap.length + 1;
-        handicap[length-1] = cast(Piece)(p + offset);
+        if (handicap[p] == 0)
+        {
+            writefln("All %s pieces already handicapped");
+            return 1;
+        }
+        handicap[p] -= 1;
     }
+
+    Piece[][2] pieces;
+    for (int i=Piece.BELEPHANT; i > Piece.EMPTY; i--)
+    {
+        int pside = (i < Piece.BRABBIT) ? Side.WHITE : Side.BLACK;
+        for (int j=0; j < handicap[i]; j++)
+        {
+            pieces[pside].length = pieces[pside].length + 1;
+            pieces[pside][length-1] = cast(Piece)i;
+        }
+    }
+
+    writef("Playing ");
+    int rnum = 0;
+    for (int i = 0; i < pieces[0].length; i++)
+    {
+        if (pieces[0][i] == Piece.WRABBIT)
+            rnum++;
+        else
+            writef("%s", ".RCDHME"[pieces[0][i]]);
+    }
+    writef("%dR vs ", rnum);
+    rnum = 0;
+    for (int i = 0; i < pieces[1].length; i++)
+    {
+        if (pieces[1][i] == Piece.BRABBIT)
+            rnum++;
+        else
+            writef("%s", ".......rcdhme"[pieces[1][i]]);
+    }
+    writefln("%dr", rnum);
 
     for (int i=0; i < tests; i++)
     {
         pos.clear();
-        if (side == Side.WHITE)
-        {
-            setgen.setup_handicap(handicap, pos);
-            setgen.setup_board(Side.BLACK, pos);
-        } else {
-            setgen.setup_board(Side.WHITE, pos);
-            setgen.setup_handicap(handicap, pos);
-        }
+        setgen.setup_handicap(pieces[0], pos);
+        setgen.setup_handicap(pieces[1], pos);
         PlayoutResult result = playout_steps(pos);
         if (result.endscore == 1)
             wins += 1;
     }
 
     writefln("Win percentage for gold %.2f%%", (cast(double)wins / tests)*100);
-    writefln("%d tests with %d wins", tests, wins);
+    writefln("%d playouts with %d wins", tests, wins);
 
     return 0;
 }
