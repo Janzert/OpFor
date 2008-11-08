cbb01e13902a89dc2989b180e03eb89cd6559653
janzert@janzert.com <>
2008-11-08 17:02:08 -0500

    Simple build script

---
+++ build.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+from subprocess import Popen
+
+sourcefiles = ["bot_opfor.d", "aeibot.d", "alphabeta.d", "logging.d",
+        "goalsearch.d", "position.d", "setupboard.d", "staticeval.d",
+        "trapmoves.d", "zobristkeys.d"]
+if len(sys.argv) > 1:
+    cmd = ["dmd"] + sys.argv[1:] + sourcefiles
+else:
+    cmd = ["dmd", "-release", "-O", "-inline"] + sourcefiles
+print " ".join(cmd)
+
+p = Popen(cmd)
+p.wait()
+
